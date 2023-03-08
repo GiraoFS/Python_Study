11ec32ff255a29211432db72287442717014c091
GiraoFS <girao.fs@gmail.com>
2023-03-07 21:51:41 -0300

    Starting hangman game logic

--- Alura/exercicios/jogo_forca/forca.py
+++ Alura/exercicios/jogo_forca/forca.py
@@ -1,9 +1,26 @@
-
-
 def jogar():
     print('*********************************')
     print('***Bem vindo ao jogo de Forca!***')
     print('*********************************')
+
+    palavra_secreta = 'banana'
+    letras_acertadas = ["_","_","_","_","_","_"]
+
+    enforcou = False
+    acertou = False
+
+    print(letras_acertadas)
+
+    while not enforcou and not acertou:
+        chute = input('Qual letra você deseja tentar? ').strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if chute.upper() == letra.upper():
+                letras_acertadas[index] = letra
+            index += 1
+
+        print(letras_acertadas)
 
     print('Fim do jogo!')
 
